40092f7c7ace25432b84cf27d4621de086f7b231
Anna Jantzen <annajantzen@Annas-MacBook-Pro.local>
2024-01-20 16:50:03 -0600

    added js

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+$(document).ready(function () {
+// sets the current date and time in the header
+  var currentDate = dayjs().format("MMM DD, YYYY, h:mm:ss a");
+  $("#currentDay").html(currentDate);
+
+  $(".saveBtn").on("click", function(event) {
+    var typedText 
+    var timeClock
+    if ($(event.target).attr("class") === "fas fa-save") {
+      typedText = $(event.target).parent().siblings(".description").val();
+      }
+      else {
+        typedText = $(this).siblings(".description").val();
+      }
+
+      timeClock = $(this).parent().attr("id");
+      
+    localStorage.setItem(timeClock, typedText); 
+    // sets items in the local storage
+  })
+
+  function trackTime (){ 
+    var currentTime = dayjs().hour()
+    
+    $(".time-block").each(function (){
+      var time = parseInt($(this).attr("id").split("hour-")[1]);
+      var event = localStorage.getItem($(this).attr("id"));
+      var textArea = $(this).children("textarea");
+      textArea.val(event)
+
+      if (time < currentTime) {
+        $(this).addClass('past');
+        $(this).removeClass('future');
+        $(this).removeClass('present');
+      }
+      else if (time === currentTime) {
+        $(this).addClass('present');
+        $(this).removeClass('future');
+        $(this).removeClass('past');  
+      }
+      else {
+        $(this).addClass('future');
+        $(this).removeClass('present');
+        $(this).removeClass('past');
+      }
+    })
+  }
+  trackTime()
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
